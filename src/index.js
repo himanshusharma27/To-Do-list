c11f1fa677eb982b39b6a3e4fe884c1b7fc66b5d
Himanshu <himanshuitm27@gmail.com>
2023-07-02 22:27:36 +0530

    some

--- src/index.js
+++ src/index.js
@@ -14,3 +14,19 @@ const valueInput = document.querySelector('.value');
 valueInput.addEventListener('keypress', handleKeyPress);
 const clearBtn = document.querySelector('.clearBtn');
 clearBtn.addEventListener('click', handleClearButtonClick);
+
+
+function addTask() {
+  var taskInput = document.getElementById("taskInput");
+  var taskList = document.getElementById("taskList");
+  
+  var task = taskInput.value.trim();
+  
+  if (task !== "") {
+    var listItem = document.createElement("li");
+    listItem.appendChild(document.createTextNode(task));
+    taskList.appendChild(listItem);
+    
+    taskInput.value = "";
+  }
+}
